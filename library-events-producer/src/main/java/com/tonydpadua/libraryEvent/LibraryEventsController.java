50431f4bce195bdfea7017a56972691a0eb6b61c
tonydpadua <tonydpadua1999@gmail.com>
2021-07-14 23:25:04 -0300

    Build LibrarEvents Producer using KafkaTemplate - Approach 1 - Part 2

--- library-events-producer/src/main/java/com/tonydpadua/libraryEvent/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/tonydpadua/libraryEvent/LibraryEventsController.java
@@ -1,5 +1,8 @@
 package com.tonydpadua.libraryEvent;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tonydpadua.producer.LibraryEventProducer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,12 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LibraryEventsController {
 
+
+    private final LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> save(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> save(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
         //invoke kafka producer
+        this.libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
